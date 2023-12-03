561a7af155d83256ef89b69416dc07905fec7dec
Darya Buzhynskaya <Bujinski4u@gmail.com>
2023-12-03 16:52:58 +0000

    prob five

--- js/app.js
+++ js/app.js
@@ -37,18 +37,8 @@ function sumAndMultiply(num1, num2, num3) {
 
 testSumAndMultiply(4,7,5);
 
-/////////////////////////////////////
+// problem 4
 
-/* Problem 4
-Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
-
-"2,3,4 was passed in as an array of numbers, and 9 is their sum."
-
-IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do addition, use your sum() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
-
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
-
-// Write your code here
 function sumArray(numbers) {
 
   let sumResult = numbers.reduce((accumulator, currentValue) => sum(accumulator, currentValue)[0], 0);
@@ -62,25 +52,18 @@ let testArray = [2, 3, 4];
 
 testSumArray(testArray);
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
+// problem 5
 
-/////////////////////////////////////
-/* Problem 5
-Write a function called multiplyArray() that takes an array of numbers as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+function multiplyArray(numbers) {
 
-"The numbers 2,3,4 have a product of 24."
+  let productResult = numbers.reduce((accumulator, currentValue) => multiply(accumulator, currentValue)[0], 1);
 
-IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. This function should handle an array containing three elements. However, you may continue to use the + operator for string concatenation.
+  let arrayString = `The numbers ${numbers.join(',')} have a product of ${productResult}.`;
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
-
-// Write your code here
-function multiplyArray(multArr) { //eslint-disable-line
-
+  return [productResult, arrayString];
 }
 
-// Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
